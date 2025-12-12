a34a523a10bcb61bde01e2663c381a75b9ee1925
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-12 17:58:32 +0700

    Add debug export

--- scripts/run_moe.sh
+++ scripts/run_moe.sh
@@ -1,3 +1,6 @@
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_SHOW_CPP_STACKTRACES=1
+
 python src/moe/train_cli.py \
     --train_path dataset/asc/rest14/train.json \
     --test_path dataset/asc/rest14/test.json \
