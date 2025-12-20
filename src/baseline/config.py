d48912002046c6378dbe3666f26cafecd00fddfe
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-20 22:42:02 +0700

    debug import shared

--- src/baseline/config.py
+++ src/baseline/config.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, asdict
 from typing import Any, Dict
-from utils import BaseTrainConfig
+from shared import BaseTrainConfig
 
 
 @dataclass
