f0c7293f404b673aa4d134311249d395b6896e79
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-20 22:48:23 +0700

    Update __init__

--- src/shared/__init__.py
+++ src/shared/__init__.py
@@ -1,34 +1,97 @@
-from .datasets import AspectSentimentDataset, AspectSentimentDatasetFromSamples
-from .optim import build_optimizer_and_scheduler
-from .plotting import plot_history, _print_confusion_matrix
-from .utils import cleanup_cuda, _parse_int_list, _parse_str_list, DEVICE
-from .seed import set_all_seeds, set_determinism, seed_worker, make_train_loader_with_seed
+# datasets
+from .datasets import (
+    AspectSentimentDataset,
+    AspectSentimentDatasetFromSamples,
+)
+
+# model
 from .build_model import BaseBertConcatClassifier
-from .logit import logits_to_metrics, collect_test_logits
-from .config import BaseTrainConfig
+
+# training engine
+from .engine import (
+    eval_model,
+    run_training_loop,
+)
+
+# training workflows
 from .benchmarks import run_benchmark_kfold_plus_full
 from .full_train import train_full_multi_seed_then_test_generic
 
+# optimization
+from .optim import build_optimizer_and_scheduler
+
+# metrics and logits
+from .logit import (
+    logits_to_metrics,
+    collect_test_logits,
+)
+
+# config
+from .config import BaseTrainConfig
+
+# plotting
+from .plotting import (
+    plot_history,
+    _print_confusion_matrix,
+)
+
+# utils
+from .utils import (
+    DEVICE,
+    cleanup_cuda,
+    _parse_int_list,
+    _parse_str_list,
+)
+
+# seeding
+from .seed import (
+    set_all_seeds,
+    set_determinism,
+    seed_worker,
+    make_train_loader_with_seed,
+)
+
 __all__ = [
+    # constants
     "DEVICE",
+
+    # datasets
     "AspectSentimentDataset",
     "AspectSentimentDatasetFromSamples",
+
+    # model
+    "BaseBertConcatClassifier",
+
+    # config
+    "BaseTrainConfig",
+
+    # optimization
     "build_optimizer_and_scheduler",
+
+    # training engine
+    "eval_model",
+    "run_training_loop",
+
+    # workflows
+    "run_benchmark_kfold_plus_full",
+    "train_full_multi_seed_then_test_generic",
+
+    # metrics
+    "logits_to_metrics",
+    "collect_test_logits",
+
+    # plotting
     "plot_history",
     "_print_confusion_matrix",
+
+    # utils
     "cleanup_cuda",
-    "set_all_seeds", 
+    "_parse_int_list",
+    "_parse_str_list",
+
+    # seeding
+    "set_all_seeds",
     "set_determinism",
     "seed_worker",
     "make_train_loader_with_seed",
-    "logits_to_metrics",
-    "collect_test_logits",
-    "BaseTrainConfig",
-    "BaseBertConcatClassifier"
-    "_mean_std",
-    "_parse_int_list",
-    "_parse_str_list",
-    "_aggregate_confusions",
-    "train_full_multi_seed_then_test_generic",
-    "run_benchmark_kfold_plus_full",
 ]
