edbb3c1119ef6ce8b202c387942879a20907d3f3
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-21 21:50:17 +0700

    Add debug log

--- src/moe_head/config.py
+++ src/moe_head/config.py
@@ -17,4 +17,7 @@ def build_multi_moe_config(args: argparse.Namespace) -> Optional[MultiMoEConfig]
     if not bool(getattr(args, "use_moe", False)):
         return None
     args_dict = _filter_config_kwargs(vars(args), MultiMoEConfig)
+    print("ARGS")
+    print(args_dict)
+    print()
     return MultiMoEConfig(**args_dict)
