2129e274770e5d494972f1931bbe9b32453742c1
Hoang Vo <vohoang2204@gmail.com>
2025-12-25 09:01:17 +0700

    Update log for mof

--- src/mof/cli.py
+++ src/mof/cli.py
@@ -113,7 +113,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--mof_logit_clamp", type=float, default=0.0)
 
     parser.add_argument("--mof_debug", action="store_true")
-    parser.add_argument("--mof_debug_every", type=int, default=200)
+    parser.add_argument("--mof_debug_every", type=int, default=100)
     parser.add_argument("--mof_debug_max_batch", type=int, default=1)
     parser.add_argument("--mof_debug_max_experts", type=int, default=0)
 
