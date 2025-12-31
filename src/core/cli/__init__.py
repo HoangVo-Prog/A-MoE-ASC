5dc50780156949c0077b81cd6b65b581a8924fe7
Hoang Vo~ <vohoang2204@gmail.com>
2025-12-31 11:20:35 +0700

    Update

--- src/core/cli/__init__.py
+++ src/core/cli/__init__.py
@@ -15,7 +15,7 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument("--train_batch_size", type=int, default=16)
     parser.add_argument("--eval_batch_size", type=int, default=32)
-    parer.add_argument("--test_batch_size", type=int, default=32)
+    parser.add_argument("--test_batch_size", type=int, default=32)
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--lr", type=float, default=2e-5)
     parser.add_argument("--dropout", type=float, default=0.1)
