f04a438bf3e7e128abb051010bb7a63ae65d63b4
Hoang Vo~ <vohoang2204@gmail.com>
2026-01-19 15:07:38 +0700

    Update general

--- src/core/utils/general.py
+++ src/core/utils/general.py
@@ -150,7 +150,7 @@ def collect_test_logits(
     labels_chunks = []
 
     for batch in test_loader:
-        batch = {k: v.to(DEVICE) for k, v in batch.items()}
+        batch = {k: (v.to(DEVICE) if torch.is_tensor(v) else v) for k, v in batch.items()}
         outputs = model(
             input_ids_sent=batch["input_ids_sent"],
             attention_mask_sent=batch["attention_mask_sent"],
